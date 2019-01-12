15a2d9020e649f4d8e897b074e85513fe761dd35
Fer Hernandez <fh.1111@gmail.com>
2019-01-12 20:25:09 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,16 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id])
   end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+
 end
