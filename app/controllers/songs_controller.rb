b7f282ec38b33f6b0a5d1e63e5a186e9c4b2980e
Fer Hernandez <fh.1111@gmail.com>
2019-01-12 20:35:56 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,5 +3,14 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    @song.save
+    redirect_to song_path(@song)
+  end
 
 end
