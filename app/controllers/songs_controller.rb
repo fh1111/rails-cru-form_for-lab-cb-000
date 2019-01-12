36935c40c28ebca78b54b19db066a66ec34ef5eb
Fer Hernandez <fh.1111@gmail.com>
2019-01-12 20:37:16 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,4 +13,11 @@ class SongsController < ApplicationController
     redirect_to song_path(@song)
   end
 
+
+
+  private
+
+		def song_params
+			params.require(:song).permit(:name, :artist_id, :genre_id)
+		end
 end
